4fd4d9eec69d53d5f3c9353824c48b5109dc2c9b
Igor Avdeev <igor.avdeev@gmail.com>
2018-03-30 19:47:14 +0300

    start homework-7

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+    version = "1.4.0"
+    project = "infra-198013"
+    region = "europe-west1"
+}
+
+resource "google_compute_instance" "app" {
+  name = "reddit-app"
+  machine_type = "g1-small"
+  zone = "europe-west1-b"
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "reddit-base"
+    }
+  }
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+
+metadata {
+ ssh-keys = "appuser:${file("~/.ssh/id_rsa.pub")}"
+ }
+
+
+
+}
+
